442d978dc0542a642b11623fdc6f60b917d4a3b2
naeem <naeemislamjr182@gmail.com>
2023-09-04 00:56:11 +0600

    local storage handling

---
+++ storage.js
@@ -0,0 +1,10 @@
+const hello = localStorage.getItem('hello')
+console.log(hello)
+const load = () => {
+
+    localStorage.setItem('age', 23)
+
+
+}
+const loadage = localStorage.getItem('age')
+console.log(loadage)
